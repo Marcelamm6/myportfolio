89e738bc07d828541d22a52348d3881bb8178088
Marcela Borges Corrêa <marcelamm6@gmail.com>
2024-05-21 11:20:10 -0300

    travel lists rendered

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,93 +1,59 @@
-import { useState } from "react";
-
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
 ];
 
-export default function App(){
-  return (
-    <div>
-      <Steps />
-      <Steps />
-      {/* <Counter /> */}
-    </div>
-  )
+
+export default function App() {
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
 }
 
-// function Counter() {
-//   const [count, setCount] = useState(0);
-//   const [stepDay, setStepDay] = useState(1);
+function Logo (){
+  return <h1> Far Away</h1>
+}
 
-//   const date = new Date("june 21 2027")
-//   date.setDate(date.getDate() + count)
-
-//   return (
-//     <>
-//     <div className="test">
-//       <div>
-//         <button onClick={() => setStepDay((c) => c - 1)}>-</button>
-//         <span>Step: {stepDay}</span>
-//         <button onClick={() => setStepDay((c) => c + 1)}>+</button>
-//       </div>
-//     </div>
-//     <div className="test">
-//       <div>
-//         <button onClick={() => setCount((c) => c - stepDay)}>-</button>
-//         <span>Count: {count}</span>
-//         <button onClick={() => setCount((c) => c + stepDay)}>+</button>
-//       </div>
-//     </div>
-
-//     <p className="test">
-//       <span>{count === 0 ? "Today is" : count > 0 ? `${count} days from today is` : `${Math.abs(count)} days ago was `}</span>
-//       <span>{date.toDateString()}</span>
-//     </p>
-//     </>
-//   )
-
-// }
-
-
-
-function Steps() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true)
-
-  function handlePrevious(){
-    if(step > 1) setStep((s) => s - 1);
-  }
-  function handleNext(){
-    if(step < 3) setStep((s) => s + 1);
-  }
-
+function Form () {
   return (
-    <div>
-      <button className="close" onClick={()=>setIsOpen((is) => !is)}>&times;</button>
-
-      { isOpen && (
-        <div className="steps">
-        <div className="numbers">
-          <div className={step >= 1 ? "active" : ""}>1</div>
-          <div className={step >= 2 ? "active" : ""}>2</div>
-          <div className={step >= 3 ? "active" : ""}>3</div>
-        </div>
-
-        <p className="message">
-          Step {step}: {messages[step - 1]}
-        </p>
-
-        <div className="buttons">
-          <button onClick={handlePrevious} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-            Previous
-          </button>
-          <button onClick={handleNext}>Next</button>
-        </div>
-      </div>
-      )}
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
     </div>
   );
 }
 
+function PackingList() {
+  return ( 
+  <div className="list">
+  <ul>
+    {initialItems.map((item)=> (
+    <Item item={item} />
+    ))}
+  </ul>
+  </div>
+  );
+}
 
+function Item({item}){
+  return <li>
+    <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+      {item.quantity}
+      {item.description}
+    </span>
+    <button>❌</button>
+    </li>
+}
+
+function Stats(){
+  return (
+    <footer className="stats">
+      <em>
+        You have X items on your list and you already packed X (X%)
+      </em>
+    </footer>
+  )
+}
